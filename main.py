6b433589a579f426b3e2d2e47cfe82fec84514d8
Alberto Polanco <prmus10@gmail.com>
2020-08-24 01:22:19 -0500

    Here we go again

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from flask import Flask
 from flask_cors import CORS
-import apineo4j
+from apineo4j import apineo4j
 
 app = Flask(__name__)
 PORT = 5000
 DEBUG = False
 CORS(app)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return "<h1>Not Found</h1>"
 
+
 @app.route("/", methods=['GET'])
 def hello():
-    return "<h1>Ingresa la palabra a buscar en la barra de direcciones</h1><p>https://graphora.herokuapp.com/<Palabra></p>"
+    return "<h1>Ingresa la palabra a buscar en la barra de direcciones</h1><p>https://graphora.herokuapp.com/'Palabra'</p>"
+
 
 @app.route("/<word>", methods=['GET'])
 def getJSON(word):
     data = apineo4j.getData(word)
     
-    if (data != "[]"):
+    if data != "[]":
         return data
     else:
         return "<h1>Palabra invalida</h1>"
+
 
 if __name__ == "__main__":
     app.run(port=PORT, debug=DEBUG)
